a79edf2f37f47d7a384a41b7480c9bad9931c153
AioliaKnight <orzjzt@gmail.com>
2025-03-11 14:07:33 +0800

    fix: update Speed Insights import path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { homeMetadata } from './metadata'
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Suspense } from 'react'
 import { Toaster } from 'react-hot-toast'
 
