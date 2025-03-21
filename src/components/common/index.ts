9668c753dae04a05157df1405a065938b80c19b7
AioliaKnight <orzjzt@gmail.com>
2025-03-21 16:33:22 +0800

    新增 common 組件索引：創建 index.ts 提供統一導出，方便引用所有通用 UI 組件

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,12 +1,22 @@
-// 統一導出所有通用組件
-export { default as Button } from './Button';
-export { default as Card } from './Card';
-export { default as OptimizedImage } from './OptimizedImage';
-export { default as Logo } from './Logo';
-export { default as Loading, Spinner } from './Loading';
-export { default as Container } from './Container';
-export { default as Section } from './Section';
-export { default as Divider } from './Divider';
-export { default as Tabs, Tab } from './Tabs';
-export { default as Alert } from './Alert';
+/**
+ * 共用組件導出文件
+ * 集中導出所有通用UI組件，便於引用
+ */
 
+// 加載與等待指示器
+export { default as Loading, Spinner } from './Loading';
+
+// 品牌標誌
+export { default as Logo } from './Logo';
+
+// 圖片優化組件
+export { default as OptimizedImage } from './OptimizedImage';
+
+// 頁面標頭
+export { default as PageHeader } from './PageHeader';
+
+// 表單按鈕
+export { default as SubmitButton } from './SubmitButton';
+
+// 呼籲行動區塊
+export { default as CTASection } from './CTASection'; 
