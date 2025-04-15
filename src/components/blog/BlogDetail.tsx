2a5c9f4f498ead5a600cfccf707abf9bd47f3ebc
AioliaKnight <orzjzt@gmail.com>
2025-04-15 17:20:00 +0800

    blog功能

--- src/components/blog/BlogDetail.tsx
+++ src/components/blog/BlogDetail.tsx
@@ -94,21 +94,37 @@ const BlogDetail: React.FC<BlogDetailProps> = ({ post }) => {
     setShowShareMenu(prev => !prev)
   }
 
+  // 獲取完整的文章網址（包含網站域名）
+  const getFullArticleUrl = () => {
+    // 獲取站點URL，如果未定義則使用當前窗口位置
+    const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 
+                    (typeof window !== 'undefined' ? window.location.origin : 'https://www.aideamed.com')
+    // 使用window.location.pathname獲取當前路徑，確保始終使用當前頁面的實際URL
+    const path = typeof window !== 'undefined' ? window.location.pathname : `/blog/${post.slug}`
+    return `${siteUrl}${path}`
+  }
+
   const shareOnFacebook = () => {
-    const url = encodeURIComponent(window.location.href)
+    const url = encodeURIComponent(getFullArticleUrl())
     window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}`, '_blank')
     setShowShareMenu(false)
   }
 
   const shareOnTwitter = () => {
-    const url = encodeURIComponent(window.location.href)
+    const url = encodeURIComponent(getFullArticleUrl())
     const text = encodeURIComponent(post.title)
     window.open(`https://twitter.com/intent/tweet?url=${url}&text=${text}`, '_blank')
     setShowShareMenu(false)
   }
 
+  const shareOnLine = () => {
+    const url = encodeURIComponent(getFullArticleUrl())
+    window.open(`https://social-plugins.line.me/lineit/share?url=${url}`, '_blank')
+    setShowShareMenu(false)
+  }
+
   const copyLink = () => {
-    navigator.clipboard.writeText(window.location.href)
+    navigator.clipboard.writeText(getFullArticleUrl())
     setShowShareMenu(false)
     toast.success("連結已複製到剪貼簿", {
       duration: 2000
@@ -319,6 +335,16 @@ const BlogDetail: React.FC<BlogDetailProps> = ({ post }) => {
                       >
                         <TwitterIcon className="h-4 w-4 mr-2" />
                         分享到 Twitter
+                      </button>
+                      <button
+                        onClick={shareOnLine}
+                        className={SHARE_BUTTON_STYLES}
+                        role="menuitem"
+                      >
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-4 w-4 mr-2 fill-current">
+                          <path d="M19.365 9.863c.349 0 .63.285.63.631 0 .345-.281.63-.63.63H17.61v1.125h1.755c.349 0 .63.283.63.63 0 .344-.281.629-.63.629h-2.386c-.345 0-.627-.285-.627-.629V8.108c0-.345.282-.63.63-.63h2.386c.346 0 .627.285.627.63 0 .349-.281.63-.63.63H17.61v1.125h1.755zm-3.855 3.016c0 .27-.174.51-.433.596-.065.021-.133.031-.199.031-.211 0-.391-.09-.51-.25l-2.443-3.317v2.94c0 .344-.279.629-.631.629-.346 0-.626-.285-.626-.629V8.108c0-.27.173-.51.43-.595.066-.022.136-.033.194-.033.195 0 .375.105.495.254l2.462 3.33V8.108c0-.345.282-.63.63-.63.345 0 .63.285.63.63v4.771zm-5.741 0c0 .344-.282.629-.631.629-.345 0-.627-.285-.627-.629V8.108c0-.345.282-.63.63-.63.346 0 .628.285.628.63v4.771zm-2.466.629H4.917c-.345 0-.63-.285-.63-.629V8.108c0-.345.285-.63.63-.63.348 0 .63.285.63.63v4.141h1.756c.348 0 .629.283.629.63 0 .344-.282.629-.629.629M24 10.314C24 4.943 18.615.572 12 .572S0 4.943 0 10.314c0 4.811 4.27 8.842 10.035 9.608.391.082.923.258 1.058.59.12.301.079.766.038 1.08l-.164 1.02c-.045.301-.24 1.186 1.049.645 1.291-.539 6.916-4.078 9.436-6.975C23.176 14.393 24 12.458 24 10.314"/>
+                        </svg>
+                        分享到 LINE
                       </button>
                       <button
                         onClick={copyLink}
